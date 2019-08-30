def3f774beb4a39be193f257375cf45797ab5f39
Peter Hironaka <peterhironaka@Peters-MacBook-Pro.local>
2019-08-30 11:26:10 -0700

    fixed header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { StaticQuery, Link } from "gatsby"
 import { graphql } from "gatsby"
-
 import Search from "./search"
 
 const Header = () => (
@@ -14,12 +13,36 @@ const Header = () => (
       }
     `}
     render={data => (
-      <header>
-        ... header stuff...
-        <Search searchIndex={data.siteSearchIndex.index} />
-      </header>
-    )}
-  />
-)
+ 
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+            <Search searchIndex={data.siteSearchIndex.index} />
 
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+    )}
+    />
+  )
 export default Header
