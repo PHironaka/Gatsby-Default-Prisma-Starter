ad258896dd65ea902bf04a98baf8e0e76032f753
Peter Hironaka <peterhironaka@Peters-MacBook-Pro.local>
2019-09-04 11:57:29 -0700

    removed default icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,6 @@ module.exports = {
         background_color: `#000`,
         theme_color: `#000`,
         display: `standalone`,
-        icon: `src/images/pete-icon.png`, // This path is relative to the root of the site.
         icons: [
           {
             src: `../assets/android-chrome-192x192.png`,
